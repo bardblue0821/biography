fcbe28e01bbe04cde025c815fcf3522f6f68a634
bardblue0821 <bardblue0821@gmail.com>
2025-08-27 22:30:31 +0900

    welcome message

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
-import Landing from './components/Landing';
-import Career from './components/Career';
-import Portfolio from './components/Portfolio';
-import Hobby from './components/Hobby';
-import Books from './components/Books';
-import Contact from './components/Contact';
+
+import React from 'react';
+import bgImage from '/src/assets/wolca_newspaper.jpg';
+import iconImage from '/src/assets/icon.jpg';
+
+
 
 function App() {
+  // ページロード後に.fade-inへ.loadedクラスを追加
+  React.useEffect(() => {
+    const main = document.querySelector('.fade-in');
+    setTimeout(() => {
+      main?.classList.add('loaded');
+    }, 1000);
+  }, []);
   return (
-    <main>
-      <Landing />
-      <Career />
-      <Portfolio />
-      <Hobby />
-      <Books />
-      <Contact />
+    <main className="fade-in relative">
+      <div className="fade-black absolute inset-0 z-50 pointer-events-none flex items-center justify-center">
+        <span className="welcome-text text-white text-5xl font-bold tracking-wide">Welcome</span>
+      </div>
+      <section className="relative h-screen w-full overflow-hidden">
+        <img
+        src={bgImage}
+        alt="背景"
+        className="w-full h-full object-cover brightness-50"
+        />
+      </section>
+
+      <section>
+        <img
+        src={iconImage}
+        alt="アイコン"
+        className="absolute left-8 top-1/2 -translate-y-1/2 w-64 h-64 object-cover rounded-full border-4 border-white shadow-lg"
+        />
+      </section>
     </main>
   );
 }
