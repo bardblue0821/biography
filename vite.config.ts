2f1b293ba160b07b4068d9f6d4f80f4769a994cf
bardblue0821 <bardblue0821@gmail.com>
2025-08-30 16:05:25 +0900

    Modify base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/biography/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
